da42027ddec68db55a7d0af420e47fff654a49cc
Yugant Singh <yugantsingh9651@gmail.com>
2026-03-31 13:03:40 +0530

    fix some bugs

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,29 +1,42 @@
 import express from 'express'
-import {authRouter} from '../src/routes/auth.routes.js'
-import itemRouter from '../src/routes/item.routes.js'
-import collectionRouter from '../src/routes/collection.routes.js'
-import searchRouter from '../src/routes/search.routes.js'
-import graphRouter from '../src/routes/graphRoutes.js'
+import { authRouter } from './routes/auth.routes.js'
+import itemRouter from './routes/item.routes.js'
+import collectionRouter from './routes/collection.routes.js'
+import searchRouter from './routes/search.routes.js'
+import graphRouter from './routes/graphRoutes.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
+
 app.use(express.json())
-app.use(express.static("./public"))
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "https://second-brain-ps3j.onrender.com",
     "chrome-extension://omlhmjibninhndmkbpopjakfjjhhemdi"
   ],
   credentials: true
 }))
 app.use(cookieParser())
-app.use('/api/auth',authRouter)
-app.use('/api/items',itemRouter)
-app.use('/api/collections',collectionRouter)
-app.use("/api/search", searchRouter);
-app.use("/api/graph", graphRouter);
 
+app.use('/api/auth', authRouter)
+app.use('/api/items', itemRouter)
+app.use('/api/collections', collectionRouter)
+app.use("/api/search", searchRouter)
+app.use("/api/graph", graphRouter)
 
+// Static files
+app.use(express.static(path.join(__dirname, '../public')))
 
+// Refresh fix — LAST mein
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+})
 
 export default app
